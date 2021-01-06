3d125377f3430b45a08f8b352f08cc53adac316e
CaitSith2 <d_good@caitsith2.com>
2021-01-06 11:15:35 -0800

    Fix bugs that prevent seed rolling from continuing if too many plando items are placed.

--- Rom.py
+++ Rom.py
@@ -2249,7 +2249,7 @@ def write_strings(rom, world, player, team):
         local_random.shuffle(items_to_hint)
         hint_count = 5 if world.shuffle[player] not in ['vanilla', 'dungeonssimple', 'dungeonsfull'] else 8
         hint_count += 2 if world.doorShuffle[player] == 'crossed' else 0
-        while hint_count > 0:
+        while hint_count > 0 and items_to_hint:
             this_item = items_to_hint.pop(0)
             this_location = world.find_items(this_item, player)
             local_random.shuffle(this_location)
