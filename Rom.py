ff8ab75e281c9c66867addcd5b86a96d889704ae
CaitSith2 <d_good@caitsith2.com>
2021-01-22 01:50:26 -0800

    Base rom update

--- Rom.py
+++ Rom.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'da642af46c26e0a7fd0c0dbcde440dc9'
+RANDOMIZERBASEHASH = '9c463f07a4cd34a41de8b86921da41a7'
 
 import io
 import itertools
