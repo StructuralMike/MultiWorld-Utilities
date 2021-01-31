6bec4d68aa6b9b88c8e43c30d9cecfccf70e1491
CaitSith2 <d_good@caitsith2.com>
2021-01-31 00:51:48 -0800

    Triforce at pedestal / Trinexx now correctly counts towards final total.

--- Rom.py
+++ Rom.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '5c7b48f433149d10e177dc0a0bcce76b'
+RANDOMIZERBASEHASH = '064a982195885212f38fe34ac6cb2c01'
 
 import io
 import itertools
@@ -933,8 +933,6 @@ def patch_rom(world, rom, player, team, enemized):
             rom.write_byte(0x118B87, 0xC2)
 
     credits_total = 216
-    if world.goal[player] == 'icerodhunt':  # Impossible to get 216/216 with Ice rod hunt. Most possible is 215/216.
-        credits_total -= 1
     if world.retro[player]:  # Old man cave and Take any caves will count towards collection rate.
         credits_total += 5
     if world.shop_shuffle_slots[player]:  # Potion shop only counts towards collection rate if included in the shuffle.
